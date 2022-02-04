3581aef04a2c2213a2db08345625edc9ff3e2a0b
liuxsdev <1028330144@qq.com>
2022-02-04 14:33:08 +0800

    ok

--- api/tile.py
+++ api/tile.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.responses import Response
 from PIL import Image,ImageDraw,ImageFont
 from io import BytesIO
+from os import join
 app = FastAPI()
 
 
@@ -10,32 +11,11 @@ def gen_png(x,y,z):
     font_color = (156,220,255,255)
     font_size = 40
     im = Image.new("RGBA",size,(255, 255, 255,0))
-    # font = ImageFont.truetype("./CascadiaMono.ttf", font_size)
+    font = ImageFont.truetype(join("api","CascadiaMono.ttf"), font_size)
     draw = ImageDraw.Draw(im)
-    draw.text((256/2, 256/2), "x={}\ny={}\nz={}".format(x,y,z),anchor="mm",fill=font_color)
+    draw.text((256/2, 256/2), "x={}\ny={}\nz={}".format(x,y,z),font=font,anchor="mm",fill=font_color)
     draw.rectangle([1,1,255,255],outline=(0, 0, 0, 255))
     return im
-
-# http://127.0.0.1:8000/maptile/1/2/3
-
-
-@app.get("/maptile/{x}/{y}/{z}")
-async def maptile(x: int, y: int, z: int):
-    image = BytesIO()
-    img = gen_png(x,y,z)
-    img.save(image, format="PNG")
-    image.seek(0)
-    return Response(content=image.read(), media_type="image/png")
-
-
-
-@app.get("/api/tile/{x}/{y}/{z}")
-async def maptile(x: int, y: int, z: int):
-    image = BytesIO()
-    img = gen_png(x,y,z)
-    img.save(image, format="PNG")
-    image.seek(0)
-    return Response(content=image.read(), media_type="image/png")
 
 @app.get("/api/tile")
 async def maptile(x: int, y: int, z: int):
